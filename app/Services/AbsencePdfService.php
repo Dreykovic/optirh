72d3122d62c5165912eda166b361832e482636b2
Dreykovic <amonaaudrey16@gmail.com>
2025-01-14 10:35:24 +0000

    [type-absences] chore(templates): Add absence type templates

---
+++ app/Services/AbsencePdfService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Absence;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+use App\Models\AbsenceType;
+
+class AbsencePdfService
+{
+    public function generate(Absence $leaveRequest)
+    {
+        $absenceType = AbsenceType::find($leaveRequest->absence_type_id);
+
+        // Détermine le nom du template à charger en fonction du type d'absence
+        $viewData = [
+            'leaveRequest' => $leaveRequest,
+            'absenceType' => $absenceType->label,
+        ];
+
+        // On choisit le template en fonction du type d'absence
+        switch ($absenceType->name) {
+            case 'annuel':
+                $view = 'pdf.absences.leave_request_annual';
+                break;
+            case 'maternité':
+                $view = 'pdf.absences.leave_request_maternity';
+                break;
+            default:
+                $view = 'pdf.absences.leave_request_default'; // Template par défaut si nécessaire
+                break;
+        }
+
+        // Charge la vue avec les données
+        $pdf = Pdf::loadView($view, $viewData);
+
+        // Génère et retourne le PDF
+        return $pdf->download("absence_{$leaveRequest->id}.pdf");
+    }
+}
