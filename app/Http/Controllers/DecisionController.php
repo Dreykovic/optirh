9d2acb42af48eeb3b26f82ee86237775ffd60bba
Dreykovic <amonaaudrey16@gmail.com>
2025-02-21 01:34:05 +0000

    [devs] setup for decion implementation

---
+++ app/Http/Controllers/DecisionController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Decision;
+use Illuminate\Http\Request;
+
+class DecisionController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+    }
+
+    public function show()
+    {
+        try {
+            $decision = Decision::where('state', 'current')->get();
+
+            return view('pages.admin.attendances.holidays.index', compact('holidays'));
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+
+            // Gestion des erreurs avec un message d'erreur plus propre
+            return back()->with('error', 'Une erreur s\'est produite lors du chargement des types d\'absence.');
+            // abort(500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Decision $decision)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Decision $decision)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Decision $decision)
+    {
+    }
+}
---
+++ app/Models/Decision.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Decision extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'numero',
+        'année',
+        'date',
+        'pdf',
+        'state',
+    ];
+}
