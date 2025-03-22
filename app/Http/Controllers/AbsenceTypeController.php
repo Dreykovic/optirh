16960e5b40e381a8053c13fa3f7e684aae326e8f
Dreykovic <amonaaudrey16@gmail.com>
2025-03-22 11:44:41 +0000

    [46-exception-gestion] Refactor AbsenceTypeController to simplify error handling and improve code readability

--- app/Http/Controllers/AbsenceTypeController.php
+++ app/Http/Controllers/AbsenceTypeController.php
@@ -21,17 +21,10 @@ class AbsenceTypeController extends Controller
      */
     public function index()
     {
-        try {
-            $absenceTypes = AbsenceType::all();
+        $absenceTypes = AbsenceType::all();
 
-            return view('pages.admin.attendances.types.index', compact('absenceTypes'));
-        } catch (\Throwable $th) {
-            dd($th->getMessage());
+        return view('pages.admin.attendances.types.index', compact('absenceTypes'));
 
-            // Gestion des erreurs avec un message d'erreur plus propre
-            return back()->with('error', 'Une erreur s\'est produite lors du chargement des types d\'absence.');
-            // abort(500);
-        }
     }
 
     /**
@@ -46,34 +39,23 @@ class AbsenceTypeController extends Controller
      */
     public function store(Request $request)
     {
-        try {
-            // Valider les fichiers et l'image
-            $request->validate([
-                'libelle' => 'required|string',
-                'description' => 'sometimes',
-                'type' => 'sometimes',
-            ]);
 
-            AbsenceType::create([
-                'label' => $request->input('libelle'),
-                'description' => $request->input('description'),
-                'type' => $request->input('type') ?? 'NORMAL',
-            ]);
+        // Valider les fichiers et l'image
+        $request->validate([
+            'libelle' => 'required|string',
+            'description' => 'sometimes',
+            'type' => 'sometimes',
+        ]);
 
-            // Redirection avec message de succès
-            return response()->json(['message' => 'Type Absence créé avec succès.', 'ok' => true]);
-        } catch (ValidationException $e) {
-            // Gestion des erreurs de validation
-            // return response()->json(['ok' => false, 'errors' => $e->errors(), 'message' => 'Données invalides. Veuillez vérifier votre saisie.']);
-            return response()->json(['ok' => false,
-                'message' => 'Les données fournies sont invalides.',
-                'errors' => $e->errors(), // Contient tous les messages d'erreur de validation
-            ], 422);
-        } catch (\Throwable $th) {
-            // return response()->json(['ok' => false,  'message' => 'Une erreur s\'est produite. Veuillez réessayer.'], 500);
+        AbsenceType::create([
+            'label' => $request->input('libelle'),
+            'description' => $request->input('description'),
+            'type' => $request->input('type') ?? 'NORMAL',
+        ]);
 
-            return response()->json(['ok' => false,  'message' => $th->getMessage()], 500);
-        }
+        // Redirection avec message de succès
+        return response()->json(['message' => 'Type Absence créé avec succès.', 'ok' => true]);
+
     }
 
     /**
@@ -95,34 +77,23 @@ class AbsenceTypeController extends Controller
      */
     public function update(Request $request, $absenceTypeId)
     {
-        try {
-            // Valider les fichiers et l'image
-            $request->validate([
-                'libelle' => 'required|string',
-                'description' => 'sometimes',
-                'type' => 'sometimes',
-            ]);
-            $absenceType = AbsenceType::find($absenceTypeId);
-            $absenceType->label = $request->input('libelle');
-            $absenceType->description = $request->input('description');
-            $absenceType->type = $request->input('type') ?? $absenceType->type;
 
-            $absenceType->save();
+        // Valider les fichiers et l'image
+        $request->validate([
+            'libelle' => 'required|string',
+            'description' => 'sometimes',
+            'type' => 'sometimes',
+        ]);
+        $absenceType = AbsenceType::find($absenceTypeId);
+        $absenceType->label = $request->input('libelle');
+        $absenceType->description = $request->input('description');
+        $absenceType->type = $request->input('type') ?? $absenceType->type;
 
-            // Redirection avec message de succès
-            return response()->json(['message' => 'Type Absence mis à jour avec succès.', 'ok' => true]);
-        } catch (ValidationException $e) {
-            // Gestion des erreurs de validation
-            // return response()->json(['ok' => false, 'errors' => $e->errors(), 'message' => 'Données invalides. Veuillez vérifier votre saisie.']);
-            return response()->json(['ok' => false,
-                'message' => 'Les données fournies sont invalides.',
-                'errors' => $e->errors(), // Contient tous les messages d'erreur de validation
-            ], 422);
-        } catch (\Throwable $th) {
-            // return response()->json(['ok' => false,  'message' => 'Une erreur s\'est produite. Veuillez réessayer.'], 500);
+        $absenceType->save();
 
-            return response()->json(['ok' => false,  'message' => $th->getMessage()], 500);
-        }
+        // Redirection avec message de succès
+        return response()->json(['message' => 'Type Absence mis à jour avec succès.', 'ok' => true]);
+
     }
 
     /**
@@ -130,13 +101,10 @@ class AbsenceTypeController extends Controller
      */
     public function destroy($id)
     {
-        try {
-            \DB::table('absence_types')->where('id', $id)->delete();
 
-            return response()->json(['ok' => true, 'message' => 'Le type d\absence a été retiré avec succès.']);
-        } catch (\Throwable $th) {
-            return response()->json(['ok' => false, 'message' => $th->getMessage()]);
-            // return response()->json(['ok' => false, 'message' => 'Une erreur s\'est produite. Veuillez réessayer.']);
-        }
+        \DB::table('absence_types')->where('id', $id)->delete();
+
+        return response()->json(['ok' => true, 'message' => 'Le type d\absence a été retiré avec succès.']);
+
     }
 }
